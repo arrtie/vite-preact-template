8bb1751f9480e419b866d892a83eff79bb6f6f8e
Artie <artierosser@gmail.com>
2025-05-06 16:00:13 +0000

    change server binding

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+/** @format */
+
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { host: true },
   plugins: [
     preact({
       jsxImportSource: "@emotion/react",
